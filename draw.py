b28320d756a3bb02d44bb4503d1607fbd5e1477f
Nick Brust <nickbrust@gmail.com>
2023-11-14 01:03:18 -0500

    Streamline drawing by pre-removing inviable picks

--- draw.py
+++ draw.py
@@ -10,13 +10,11 @@ Set the following env vars:
 """
 import os
 import smtplib
-from dotenv import load_dotenv
 from copy import deepcopy
 from random import randint
 from yaml import safe_load
 
 # read email from env vars
-load_dotenv()
 EMAIL = os.getenv('EMAIL')
 # read email app password from env vars (different from your "normal" password)
 EMAIL_APP_PASS = os.getenv('EMAIL_APP_PASS')
@@ -45,63 +43,42 @@ class Hat():
             party = safe_load(yam)
             self.names = [person for person in party]
 
-    def draw(self, person, drawn=None):
-        """
-        Person draws names from the hat until a valid name is drawn.
-
-        Drawing oneself or a name from one's forbidden list is invalid.
-
-        Args:
-            person (Person): person drawing the name
-            drawn (list): forbidden names already drawn by person
-
-        Returns:
-            str||bool: name drawn or False if valid name is impossible to draw
-        """
-        # use deepcopy to avoid modifying hat in-place
-        backup_hat = deepcopy(self.names)
-        # if last name in the hat is the person drawing or in their forbidden
-        if len(self.names) == 1 and (self.names[0] == person.name or self.names[0] in person.forbidden):
-            # return False to signal a full redraw
-            return False
-        # create empty drawn list on inital call of draw()
-        if drawn is None:
-            drawn = []
-        # draw a random name from the hat
-        drawn.append(self.names.pop(randint(0, len(self.names)-1)))
-        # if drawn self or forbidden
-        if drawn[-1] == person.name or drawn[-1] in person.forbidden:
-            print("draw again...")
-            # try again, keep track of previously drawn names
-            self.names = backup_hat
-            return self.draw(person, drawn)
-        # otherwise return the last (valid) drawn name
-        return drawn[-1]
-
-    def draw_names(self, party):
-        """
-        Cycles through party, having each person draw a name from the hat.
-
-        Performs redraws until a valid conclusion is reached.
-        Modifies party in-place by updating each participant's 'giftee'.
-
-        Args:
-            party (Party): list of participants (dict)
-        """
-        # use deepcopy to avoid modifying hat and party in-place (used for redraws)
-        backup_hat = deepcopy(self.names)
-        backup_party = deepcopy(party)
-        # cycle through party
+    def draw(self, party):
         for person in party.people:
-            # draw a name to be giftee
-            giftee = self.draw(person)
-            # False signals a full redraw
-            if not giftee:
-                print("redraw...")
-                self.names = backup_hat
-                return self.draw_names(backup_party)
-            # otherwise, set giftee
+            # prune hat before drawing
+            pruned = []
+            # remove person drawing
+            try:
+                self.names.remove(person.name)
+                pruned.append(person.name)
+            except ValueError as val_err:
+                pass
+            # remove forbidden
+            for name in person.forbidden:
+                try:
+                    self.names.remove(name)
+                    pruned.append(name)
+                except ValueError as val_err:
+                    pass
+            # if none left, try to reconcile
+            if len(self.names) == 0:
+                # take last pruned and try to make a viable swap
+                giftee = pruned.pop()
+                # look at party (not current person or their forbidden)
+                for other in [p for p in party.people if p.name != person.name and p.name not in person.forbidden]:
+                    # see if their giftee works for current person
+                    if giftee not in other.forbidden and (other.giftee not in person.forbidden and other.giftee != person.name):
+                        # swap
+                        new_giftee = other.giftee
+                        other.giftee = giftee
+                        giftee = new_giftee
+                        break
+            else:
+                # draw
+                giftee = self.names.pop(randint(0, len(self.names)-1))
             person.giftee = giftee
+            # replace pruned
+            self.names.extend(pruned)
 
 def email(person):
     """
@@ -110,32 +87,29 @@ def email(person):
     Only works with gmail as-written.
 
     Args:
-        person (dict): party participant's info
+        person (Person): party participant
     """
-    # break person dict into name and info
-    for name, info in person.items():
-        # build email
-        to = info['email']
-        subject = 'Secret Santa'
-        body = (f"Hello {name}!\n\n"
-                f"You are the Secret Santa for {info['giftee']}!\n\n")
+    # build email
+    subject = 'Secret Santa'
+    body = (f"Hello {person.name}!\n\n"
+            f"You are the Secret Santa for {person.giftee}!\n\n")
 
-        # bring everything together
-        email_text = (f"From: {EMAIL}\n"
-                      f"To: {to}\n"
-                      f"Subject: {subject}\n"
-                      f"{body}")
+    # bring everything together
+    email_text = (f"From: {EMAIL}\n"
+                    f"To: {person.email}\n"
+                    f"Subject: {subject}\n"
+                    f"{body}")
 
-        try:
-            # send email
-            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-            server.ehlo()
-            server.login(EMAIL, EMAIL_APP_PASS)
-            server.sendmail(EMAIL, to, email_text)
-            server.close()
-            print('Email sent!')
-        except:
-            print('Something went wrong...')
+    try:
+        # send email
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(EMAIL, EMAIL_APP_PASS)
+        server.sendmail(EMAIL, person.email, email_text)
+        server.close()
+        print('Email sent!')
+    except:
+        print('Something went wrong...')
 
 if __name__ == "__main__":
     # list of participants from PARTY yaml
@@ -144,11 +118,13 @@ if __name__ == "__main__":
     hat = Hat()
 
     # run the secret santa drawing
-    hat.draw_names(party)
+    hat.draw(party)
 
     for person in party.people:
-        print(f"{person.name} got {person.giftee}")
+        if not person.giftee:
+            print(f"Something isn't quite right. {person.name} got {person.giftee}.")
+            exit(1)
 
     # email participants
-    for person in party:
+    for person in party.people:
         email(person)
